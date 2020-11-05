1807f13d340883895fface43f935b18932dbcd42
Reza Karimi <rezakrimi@gmail.com>
2020-11-04 19:55:05 -0500

    link prediction

---
+++ salsa.py
@@ -0,0 +1,55 @@
+import networkx as nx
+import pickle
+
+def ppr(key):
+    personalization = ref_personalization.copy()
+    personalization[key] = 1.0
+    return nx.pagerank(G, personalization=personalization)
+
+with open('company_investor_undirected.pkl', 'rb') as file:
+    G = pickle.load(file)
+
+
+CIRCLE_SIZE = 50
+ref_personalization = {}
+
+for node in G.nodes:
+    if G.nodes[node]['type'] == 'investor':
+        ref_personalization[node] = 0.0
+
+# betweenness_centrality = nx.betweenness_centrality(G)
+# betweenness_centrality = sorted(betweenness_centrality.items(), key=lambda x: x[1])
+# print(betweenness_centrality)
+# print(G.nodes[betweenness_centrality[-1][0]]['type'])
+# Tencent Holdings has the highest betweenness_centrality
+
+
+# ppr for all investors
+# for key in ref_personalization:
+#     personalization = ref_personalization.copy()
+#     personalization[key] = 1.0
+#     ppr = nx.pagerank(G, personalization=personalization)
+#     print(ppr)
+
+th_ppr = ppr('Tencent Holdings')
+th_ppr = sorted(th_ppr.items(), key=lambda x: x[1], reverse=True)
+hubs = []
+for ppr in th_ppr:
+    if G.nodes[ppr[0]]['type'] == 'investor' or G.nodes[ppr[0]]['type'] == 'company_investor':
+        hubs.append(ppr[0])
+    if len(hubs) == CIRCLE_SIZE:
+        break
+auths = set([])
+for h in hubs:
+    for child in G.neighbors(h):
+        if G.nodes[child]['type'] == 'company':
+            auths.add(child)
+
+sub_graph = nx.subgraph(G, hubs+list(auths))
+print(sub_graph.number_of_nodes())
+print(sub_graph.number_of_edges())
+hubs, auths = nx.hits(sub_graph, max_iter=1000)
+auths = sorted(auths.items(), key=lambda x: x[1], reverse=True)
+for a in auths:
+    if G.nodes[a[0]]['type'] == 'company' and a[0] not in G.neighbors('Tencent Holdings'):
+        print(a[0])
